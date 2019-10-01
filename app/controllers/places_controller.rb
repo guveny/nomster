a92e677be8f7bc6b88b2d628fa09da5bf0e83172
Guven Yildiz <guven.yildiz@gmail.com>
2019-10-01 02:39:37 +0000

    lesson14@nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,16 +1,20 @@
 class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
 
-def index
-  @places = Place.all
-end
+  def new
+    @place = Place.new
+  end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
-def new
+  private
 
-@place = Place.new
-
-end
-
-
-
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
