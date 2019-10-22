70dd0ea47f756f895c6d8c6250e7eb74706318b5
Guven Yildiz <guven.yildiz@gmail.com>
2019-10-22 05:04:44 +0000

    lesson#41@nomster

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,7 +5,7 @@ class NotificationMailer < ApplicationMailer
 
            def comment_added
            
-                        mail(to: "guveny@me.com",
+                        mail(to: "guven.yildiz@gmail.com",
                                     subject: "A comment has been added to your place")
            end
 end
