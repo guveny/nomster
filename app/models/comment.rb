24bc225a0d629e2c035c3f9643938b53778b5a5d
Guven Yildiz <guven.yildiz@gmail.com>
2019-10-22 05:32:49 +0000

    lesson#43@nomster

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
 belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
 
   RATINGS = {
     'one star': '1_star',
@@ -14,6 +15,8 @@ belongs_to :user
   def humanized_rating
     RATINGS.invert[self.rating]
   end
-
+def send_comment_email
+      NotificationMailer.comment_added(self).deliver_now
+  end
   
 end
